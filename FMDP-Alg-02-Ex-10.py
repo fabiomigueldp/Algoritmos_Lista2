646863fe1c90d1fe3d1b3199c0bb99c25c84da6e
fabiomigueldp <fabiomigueldp@gmail.com>
2023-04-12 23:23:49 -0300

    Aperfeiçoamento no ex. 10.

--- FMDP-Alg-02-Ex-10.py
+++ FMDP-Alg-02-Ex-10.py
@@ -10,14 +10,18 @@ Crie um programa Python que leia o número de matrícula de um aluno e imprima o
 semestre em que ele foi matriculado. Por exemplo, um número de matrícula 182034 deve
 resultar ano 18 e semestre 2.'''
 
+# Observação
+print("OBS: Em consequência do formato do código da matrícula limitar a representação do ano somente")
+print("a dois algarismos, só é possível que o programa funcione para um determinado intervalo de 100 anos.")
+
 # Solicita o número de matrícula
 
 codMatricula = int(input("Insira o número de matrícula do aluno: "))
 
-# Limita o valor no intervalo 0<=valor<=99
+# Limita o valor no intervalo
 
 while True:
-    if codMatricula >= -999999 and codMatricula <= 999999:
+    if codMatricula >= 1001 and codMatricula <= 999999:
         break
     print("Valor inválido, tente novamente.")
     codMatricula = int(input("Insira o número de matrícula do aluno: "))
@@ -28,17 +32,20 @@ AA = codMatricula // 10000
 S = ((codMatricula // 1000) % 10)
 DDD = codMatricula % 1000
 
-# Gera o ano
+# Verifica se o valor do terceiro dígito é válido
+if S != 1 and S != 2:
+    print("Valor inválido, tente novamente.")
+else:
+    # Gera o ano
+    ano = 2000 + AA
 
-ano = "20" + str(AA)
+    # Gera semestre
+    if S == 1:
+        semestre = "1º semestre"
+    elif S == 2:
+        semestre = "2º semestre"
+        
+    # Imprime a resposta
+    
+    print(f"O aluno foi matriculado no ano de {ano}, no {semestre}, com o número {DDD}.")
 
-# Gera semestre
-
-if S == 1:
-    semestre = "1º semestre"
-elif S == 2:
-    semestre = "2º semestre"
-
-# Imprime a resposta
-
-print(f"O aluno foi matriculado no ano de {ano}, no {semestre}, com o número {DDD}.")
